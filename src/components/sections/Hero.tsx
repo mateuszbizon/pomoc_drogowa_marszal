d933704f1212ade00f5a772d28f7cd0a9f06bc7a
Mateusz Bizoń <mat-biz@wp.pl>
2025-08-26 14:01:47 +0200

    changed hero padding in mobile

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,7 +7,7 @@ import { PHONE2 } from '@/constants'
 
 function Hero() {
   return (
-    <header className='py-section-padding bg-[url(/hero_bg_small.jpg)] md:bg-[url(/hero_bg_medium.jpg)] xl:bg-[url(/hero_bg_big.jpg)] bg-cover bg-center bg-no-repeat bg-fixed relative z-0 lg:h-screen text-white'>
+    <header className='pb-section-padding pt-[calc(theme(height.company-info-height)+theme(height.nav-height)+10px)] lg:pt-section-padding bg-[url(/hero_bg_small.jpg)] md:bg-[url(/hero_bg_medium.jpg)] xl:bg-[url(/hero_bg_big.jpg)] bg-cover bg-center bg-no-repeat bg-fixed relative z-0 lg:h-screen text-white'>
         <Shadow />
         <Container>
             <div className='gap-y-8 h-full flex flex-col justify-center text-center'>

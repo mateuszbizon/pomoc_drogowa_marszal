1242604a61cd08a33e58e9a5f4e93e0e5cc917d5
Mateusz Bizoń <mat-biz@wp.pl>
2025-08-28 18:50:35 +0200

    added ul list to about section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -18,32 +18,32 @@ function About() {
                 </div>
                 <div className='space-y-10'>
                     <h3 className='bigger-text font-black text-center'>Całodobowy mobilny serwis TIR w Zgorzelcu i okolicy <br /><span className='text-primary'>Bogdan Marszał</span></h3>
-                    <div className='space-y-8 font-black'>
-                        <div className='flex gap-4 items-center'>
+                    <ul className='space-y-8 font-black'>
+                        <li className='flex gap-4 items-center'>
                             <div>
                                 <div className='size-6 rounded-full bg-primary'></div>
                             </div>
                             <p className='little-bigger-text'>24 h, "Non Stop"</p>
-                        </div>
-                        <div className='flex gap-4 items-center'>
+                        </li>
+                        <li className='flex gap-4 items-center'>
                             <div>
                                 <div className='size-6 rounded-full bg-secondary'></div>
                             </div>
                             <p className='little-bigger-text'>Dojeżdżamy do każdego miejsca w Unii Europejskiej</p>
-                        </div>
-                        <div className='flex gap-4 items-center'>
+                        </li>
+                        <li className='flex gap-4 items-center'>
                             <div>
                                 <div className='size-6 rounded-full bg-green'></div>
                             </div>
                             <p className='little-bigger-text'>Mobilny serwis samochodów osobowych oraz ciężarowych</p>
-                        </div>
-                        <div className='flex gap-4 items-center'>
+                        </li>
+                        <li className='flex gap-4 items-center'>
                             <div>
                                 <div className='size-6 rounded-full bg-red'></div>
                             </div>
                             <p className='little-bigger-text'>Naprawy stacjonarne samochodów ciężarowych i osobowych</p>
-                        </div>
-                    </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </Container>

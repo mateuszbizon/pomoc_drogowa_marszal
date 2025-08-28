fc1dab2b9dcbcca590dda33f5381b39f9fa7e82b
Mateusz Bizoń <mat-biz@wp.pl>
2025-08-28 10:18:24 +0200

    changed sub page hero h1 text

--- src/components/sections/SubPageHero.tsx
+++ src/components/sections/SubPageHero.tsx
@@ -10,13 +10,15 @@ type SubPageHeroProps = {
     description?: string
 }
 
-function SubPageHero({ title = "O nas - Mobilny Serwis TIR Zgorzelec", description = "Poznaj nas bliżej i sprawdź co możemy dla Ciebie zrobić i co nas wyróżnia." }: SubPageHeroProps) {
+function SubPageHero({ title = "O nas", description = "Poznaj nas bliżej i sprawdź co możemy dla Ciebie zrobić i co nas wyróżnia." }: SubPageHeroProps) {
   return (
     <header className='relative pb-section-padding pt-[calc(theme(height.company-info-height)+theme(height.nav-height)+10px)] lg:pt-section-padding bg-[url(/hero_bg_small.jpg)] md:bg-[url(/hero_bg_medium.jpg)] xl:bg-[url(/hero_bg_big.jpg)] bg-cover bg-center bg-no-repeat bg-fixed lg:h-screen text-white z-0'>
         <Shadow />
         <Container>
             <div className='gap-8 h-full flex flex-col justify-center text-center'>
-                <h1 className='heading1 lg:leading-18'>{title}</h1>
+                <h1 className='heading1 lg:leading-18'>
+                    {title} <br />Mobilny Serwis TIR Zgorzelec
+                </h1>
                 <p className='bigger-text'>
                     {description}
                 </p>

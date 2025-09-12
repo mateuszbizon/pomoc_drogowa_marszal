d38c0e8732804bc4a40896ae601fac97266962a7
Mateusz Bizoń <mat-biz@wp.pl>
2025-09-12 16:12:42 +0200

    added redirects to old subpgaes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: true
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: "/auto-laweta",
+                destination: "/uslugi/auto-laweta-zgorzelec",
+                permanent: true,
+            },
+            {
+                source: "/wulkanizacja-kol",
+                destination: "/uslugi/naprawa-kol-zgorzelec",
+                permanent: true,
+            },
+            {
+                source: "/serwis-klimatyzacji",
+                destination: "/uslugi/serwis-klimatyzacji-zgorzelec",
+                permanent: true,
+            },
+        ]
+    },
 };
 
 export default nextConfig;

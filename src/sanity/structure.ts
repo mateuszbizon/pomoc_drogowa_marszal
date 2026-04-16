d452d29c8fb88c75d15a081277fb1dbfa34e2f0c
Mateusz Bizoń <mat-biz@wp.pl>
2026-04-16 12:08:02 +0200

    added sanity

---
+++ src/sanity/structure.ts
@@ -0,0 +1,14 @@
+import type {StructureResolver} from 'sanity/structure'
+
+// https://www.sanity.io/docs/structure-builder-cheat-sheet
+export const structure: StructureResolver = (S) =>
+  S.list()
+    .title('Blog')
+    .items([
+      S.documentTypeListItem('post').title('Posty'),
+      S.documentTypeListItem('category').title('Kategorie'),
+      S.divider(),
+      ...S.documentTypeListItems().filter(
+        (item) => item.getId() && !['post', 'category'].includes(item.getId()!),
+      ),
+    ])

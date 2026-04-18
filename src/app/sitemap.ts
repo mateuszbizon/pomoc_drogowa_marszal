555fea06c819da8920be9e7f1613bbc96d4a887f
Mateusz Bizoń <mat-biz@wp.pl>
2026-04-18 14:16:28 +0200

    added blog links to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,25 @@
 import type { MetadataRoute } from 'next'
+import { getAllPosts } from '@/sanity/posts/getAllPosts'
+import { NAV_ITEMS } from '@/constants/navItems'
 
 const baseUrl = 'https://serwismobilnytirzgorzelec.pl/'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
+    const { posts } = await getAllPosts()
+
+    const postLinks = posts.map(post => ({
+        url: `${baseUrl}/${post.slug?.current}`,
+        lastModified: post.publishedAt ? new Date(post.publishedAt) : new Date(),
+        priority: 0.8,
+    }))
+
+    const navLinks = NAV_ITEMS.map(item => ({
+        url: item.isLink ? `${baseUrl}${item.href}` : "",
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.8,
+    }))
+
   return [
     {
       url: `${baseUrl}`,
@@ -82,5 +99,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    ...postLinks
   ]
 }

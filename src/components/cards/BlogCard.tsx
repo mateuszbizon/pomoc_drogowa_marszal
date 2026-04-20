5cc6e4b20a9f361a2cf47c1561153a271e4620fc
Mateusz Bizoń <mat-biz@wp.pl>
2026-04-20 16:56:34 +0200

    aligned height of blog cards

--- src/components/cards/BlogCard.tsx
+++ src/components/cards/BlogCard.tsx
@@ -12,7 +12,7 @@ type BlogCardProps = {
 function BlogCard({ blog }: BlogCardProps) {
   return (
     <Link href={`/blog/${blog.slug?.current}`} target='_blank' title={`Blog: ${blog.title}`}>
-        <div className='bg-background rounded-2xl space-y-5 border-2 hover:border-black transition duration-300'>
+        <div className='bg-background rounded-2xl space-y-5 border-2 hover:border-black transition duration-300 h-full flex flex-col'>
             {blog.mainImage && (
                 <div className='p-1'>
                     <figure className='relative w-full aspect-video rounded-xl overflow-hidden'>
@@ -20,9 +20,9 @@ function BlogCard({ blog }: BlogCardProps) {
                     </figure>
                 </div>
             )}
-            <div className='p-5 pt-0 space-y-5'>
+            <div className='p-5 pt-0 space-y-5 flex flex-col h-full'>
                 <p className='font-medium text-lg line-clamp-2'>{blog.title}</p>
-                <p className='flex gap-2 items-center'>Zobacz bloga <ExternalLink className='size-5' /></p>
+                <p className='flex gap-2 items-center mt-auto'>Zobacz bloga <ExternalLink className='size-5' /></p>
             </div>
         </div>
     </Link>
